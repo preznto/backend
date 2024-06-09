b718c546b14c8522c7cb0423e35b417b7a87d78a
Johyunik <whgusdlr13@naver.com>
2024-06-09 19:46:52 +0900

    first

---
+++ src/main/java/com/buyforme/buyformeAPI/BuyformeApiApplication.java
@@ -0,0 +1,13 @@
+package com.buyforme.buyformeAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyformeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyformeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buyforme/buyformeAPI/BuyformeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buyforme.buyformeAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyformeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
