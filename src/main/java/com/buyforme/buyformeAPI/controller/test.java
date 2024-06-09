d2ced32fe024ab2e2d880db4eee07ce874583da1
Johyunik <whgusdlr13@naver.com>
2024-06-09 22:42:13 +0900

    test

--- src/main/java/com/buyforme/buyformeAPI/controller/test.java
+++ src/main/java/com/buyforme/buyformeAPI/controller/test.java
@@ -1,7 +1,9 @@
 package com.buyforme.buyformeAPI.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class test {
 
     @GetMapping("/")
