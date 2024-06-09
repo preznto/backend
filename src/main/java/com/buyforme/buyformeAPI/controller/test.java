d40cb03a59ae890419c1bb188539b179d163d16b
Johyunik <whgusdlr13@naver.com>
2024-06-09 22:46:43 +0900

    test

--- src/main/java/com/buyforme/buyformeAPI/controller/test.java
+++ src/main/java/com/buyforme/buyformeAPI/controller/test.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class test {
 
-    @GetMapping("/")
+    @GetMapping("/api")
     public String test() {
         return "test2";
     }
